d6faf0159a1de4c43e57ea6264eb3db68cc3b887
Fallen_Breath <nellaforax@outlook.com>
2024-03-06 02:13:26 +0800

    improve document of `@Condition`

--- src/main/java/me/fallenbreath/conditionalmixin/api/annotation/Condition.java
+++ src/main/java/me/fallenbreath/conditionalmixin/api/annotation/Condition.java
@@ -8,33 +8,60 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Condition
 {
+	/**
+	 * Define when the type of this condition. See {@link Type} for more information
+	 */
 	Type type() default Type.MOD;
 
 	/**
-	 * A mod id if type == MOD, e.g. "lithium" or "carpet"
-	 * A full qualified mixin class name if type == MIXIN
-	 * Not used if type == TESTER
+	 * <ul>
+	 * <li>(type MOD): A mod id, e.g. "lithium" or "carpet"
+	 * <li>(type MIXIN): A full qualified mixin class name, e.g. "com.example.mod.mixins.FooBarMixin",
+	 * <li>(type TESTER): Unused</li>
+	 * </ul>
 	 */
 	String value() default "";
 
 	/**
+	 * The value is used only when type == MOD
+	 * <p>
 	 * All possible version range requirements
-	 * The value is used when type == MOD
+	 * <p>
 	 * The condition is satisfied when the testing version matches any versionPredicate, or no versionPredicate is given
-	 * e.g. ">=1.2.3"
+	 * <p>
+	 * Examples:
+	 * <ul>
+	 * <li>">=1.2.3": version >= 1.2.3</li>
+	 * <li>">=1.2.3 <1.3": version in [1.2.3, 1.3) </li>
+	 * <li>[">=1.2.3 <1.3", ">=2.0"]: version in [1.2.3, 1.3), or version >= 2.0</li>
+	 * </ul>
 	 */
 	String[] versionPredicates() default {};
 
 	/**
-	 * Specified your tester class implemented {@link ConditionTester}
-	 * The value is used when type == TESTER
+	 * The value is used only when type == TESTER
+	 * <p>
+	 * Specified your tester class that implements {@link ConditionTester}
+	 * <p>
+	 * An object of the given class will be constructed to check the condition satisfaction
 	 */
 	Class<? extends ConditionTester> tester() default ConditionTester.class;
 
 	enum Type
 	{
+		/**
+		 * Satisfies when the given mod presents, and its version satisfied the given {@link #versionPredicates}
+		 */
 		MOD,
+
+		/**
+		 * Satisfies when the specified mixin class exists, and (if it has {@link Restriction}) its {@link Restriction} is satisfied
+		 */
 		MIXIN,
+
+		/**
+		 * Satisfies the given {@link #tester} object says ok
+		 */
 		TESTER,
 	}
 }
